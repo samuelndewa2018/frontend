10b8bc17dd828f8a9d6c725cefdc50f93ea850bd
Corne Craig <cornecraig26@gmail.com>
2024-01-01 23:33:39 +0300

    vercel add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import QRCode from "react-qr-code";
+import Announcement from "./whatsapp";
 
 function App() {
+  const [qrCode, setQrCode] = useState("");
+  const [isClientReady, setIsClientReady] = useState(false);
+
+  useEffect(() => {
+    const fetchQRCode = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/qr-code");
+        const data = await response.text();
+        setQrCode(data);
+      } catch (error) {
+        console.error("Error fetching QR code:", error);
+      }
+    };
+
+    const fetchClientStatus = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/client-status"); // Replace with your server URL
+        const data = await response.json();
+        setIsClientReady(data.isClientReady);
+        console.log("client", isClientReady);
+      } catch (error) {
+        console.error("Error fetching client status:", error);
+      }
+    };
+
+    fetchQRCode();
+    fetchClientStatus();
+
+    const intervalId = setInterval(() => {
+      fetchQRCode();
+      fetchClientStatus();
+    }, 10000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!isClientReady ? (
+        <>
+          <h1>WhatsApp Web Js</h1>
+          <h1>Scan QR code</h1>
+          <QRCode value={qrCode} />
+        </>
+      ) : (
+        <Announcement />
+      )}
     </div>
   );
 }
